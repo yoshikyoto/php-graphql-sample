be876ca505cabb0500e8f095726f763d91ec4a65
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2018-12-03 17:38:43 +0900

    PHP Graphql library version up

--- public/graphql/index.php
+++ public/graphql/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 use GraphQL\Type\Definition\ObjectType;
 use GraphQL\Type\Definition\Type;
